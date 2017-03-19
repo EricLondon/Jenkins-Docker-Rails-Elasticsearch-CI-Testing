da6f9d619c019e55c721f45e33092df13ffc4cc9
Eric London <eric.london@arcadiasolutions.com>
2017-03-19 18:28:05 -0400

    break the build!

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe Person, type: :model do
     let!(:document) { Person.__elasticsearch__.client.get(get_params) }
 
     it 'indexes model data' do
+      expect(true).to eq(false)
       expect(document['_source']['id']).to eq(person.id)
       expect(document['_source']['first_name']).to eq(first_name)
       expect(document['_source']['last_name']).to eq(last_name)
